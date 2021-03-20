f73278f740c25ca347933879ac33ff7241ff4b91
Brady <bradyrcotton@gmail.com>
2021-03-19 23:01:26 -0500

    clear some comments in battlefield.py and added an output in the attack_sequence() to output who is attacking who

--- battlefield.py
+++ battlefield.py
@@ -11,8 +11,9 @@ class Battlefield:
 
     def attack_sequence(self, attacker, defender):
         defender.health -= attacker.attack
+        print(f"{attacker.type} attacks {defender.type}")
         attacker.health -= defender.attack
-
+        print(f"{defender.type} attacks {attacker.type}")
 
     def run_game(self):
 
@@ -24,9 +25,6 @@ class Battlefield:
         k = 0
 
         while i < 3:
-
-            # self.attack_sequence(self.herd.herd[i], self.fleet.fleet[i] )
-            # if i < 3:
 
             while self.herd.herd[i].health > 0 and self.fleet.fleet[i].health > 0:
                 self.attack_sequence(self.herd.herd[i], self.fleet.fleet[i])
@@ -50,24 +48,12 @@ class Battlefield:
                 print("The fleet of robots has destroyed the herd of dinosaurs")
             else:
                 print("The herd of dinosaurs has defeated the fleet of robots")
-def random_number(min_value, max_value):
-    return random.randint(min_value, max_value)
 
+
+def random_number(min_value, max_value):
+
+    return random.randint(min_value, max_value)
 
 
 Battlefield().run_game()
 
-
-    # print(Herd.herd[0])
-
-    # while robot_one.robos_health > 0 and dino_one.dinos_health > 0:
-    #     Dino.dino_attack(dino_one, robot_one)
-    #     Robot.robot_attack(robot_one, dino_one)
-    #     if robot_one.robos_health > 0:
-    #         print(robot_one.robos_health)
-    #     if dino_one.dinos_health > 0:
-    #         print(dino_one.dinos_health)
-    #     elif robot_one.robos_health < 0:
-    #         print(f"{robot_one.robos_type} is dead")
-    #     elif dino_one.dinos_health < 0:
-    #         print(f"{dino_one.dinos_type} is dead")
